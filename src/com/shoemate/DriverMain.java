4701a9004ed314e0007d8aa0e65d4b2b2ca74b41
Shoeboxam <shoeboxam@gmail.com>
2017-10-14 15:23:43 -0500

    Add specification

--- src/com/shoemate/DriverMain.java
+++ src/com/shoemate/DriverMain.java
@@ -1,9 +1,19 @@
+/* Analysis: Create Othello with support for human and AI players.
+   Design:   Collect user preferences for board size, players, and game mode (diagonal)
+             Iterate through player list until a play cannot be made
+                Continue to poll a player until a valid play is made
+                Apply othello game rules to the game board when a valid play is made
+ */
+
 package com.shoemate;
 
 public class DriverMain {
     public static void main(String[] args) {
-
+        
+        // Initialize game
         Game myGame = new Game();
+
+        // Play the game!
         myGame.start();
     }
 }
